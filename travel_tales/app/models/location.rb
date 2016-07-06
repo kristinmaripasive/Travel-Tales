471cd5b9db554ec9ff20867dcbc4553f58e1eff1
Kristin Pasive <kristin.pasive@gmail.com>
2016-07-06 16:09:45 -0400

    fixed relationships in console

--- travel_tales/app/models/location.rb
+++ travel_tales/app/models/location.rb
@@ -1,9 +1,8 @@
 class Location < ActiveRecord::Base
-  has_many :thoughts, dependent: :destroy
   validates :name, uniqueness: :true, presence: :true
-  belongs_to :user
-  has_many :comments
 
+  has_many :thoughts, dependent: :destroy
+  has_many :comments
   has_many :user_locations
   has_many :users, through: :user_locations
 end
